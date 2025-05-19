0873e4f4ddabc6cdcb0494efb66c20b078e59d71
Karl Muller <karlmuller10@gmail.com>
2025-05-19 13:25:25 -0400

    change styling to transparent

--- nvim/lua/karl/lazy/colorscheme.lua
+++ nvim/lua/karl/lazy/colorscheme.lua
@@ -4,7 +4,7 @@ return {
     require("tokyonight").setup({
       -- your configuration comes here
       -- or leave it empty to use the default settings
-      style = "storm",                  -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+      style = "night",                  -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
       transparent = true,               -- Enable this to disable setting the background color
       terminal_colors = true,           -- Configure the colors used when opening a `:terminal` in Neovim
       styles = {
@@ -13,8 +13,8 @@ return {
         comments = { italic = false },
         keywords = { italic = false },
         -- Background styles. Can be "dark", "transparent" or "normal"
-        sidebars = "dark",             -- style for sidebars, see below
-        floats = "dark",               -- style for floating windows
+        sidebars = "transparent",             -- style for sidebars, see below
+        floats = "transparent",               -- style for floating windows
       },
     })
   end
